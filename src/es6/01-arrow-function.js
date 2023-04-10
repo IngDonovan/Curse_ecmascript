c475b1a15f3e1aefc8f2d94a2d0713e79346236d
Donovan Rojas <rojasdrm03@gmail.com>
2023-04-10 16:41:57 -0500

    example arrow function

---
+++ src/es6/01-arrow-function.js
@@ -0,0 +1,14 @@
+//funciones de tipo flecha o arrow Function es una alternativa compacta a una expresion de funcion tradicional, pero es limitada y no se puede utilizar en todas las situaciones
+
+//funcion normal
+
+function square(num) {
+    return num * num;
+}
+
+//con arrow function, al ser una funcion se recomienda const
+const square = (num) => {
+    return num * num;
+}
+
+const square = num => num * num;//con el return implicito
