ba8e50322c8a26dbec8b0c656e273e4a8746e103
Donovan Rojas <rojasdrm03@gmail.com>
2023-04-11 09:37:01 -0500

    template literals and milti-line strings

---
+++ src/es6/02-strings.js
@@ -0,0 +1,19 @@
+let hello = 'Hello';
+let world = 'World';
+let epicPhrase = hello + ' ' + world + '!';
+console.log(epicPhrase);
+
+//propuesta template literals, comillas francesas ````
+let epicPhrase2 = `${hello} ${world}!`;
+console.log(epicPhrase2);
+
+//multi-line strings
+let lorem = 'esto es un string \n ' + 'esto es otra linea';
+
+//multilinea example
+let lorem2 = `Esta es una frase epica
+ la continuación de esa frase epica.
+ `;
+
+ console.log(lorem);
+ console.log(lorem2);
