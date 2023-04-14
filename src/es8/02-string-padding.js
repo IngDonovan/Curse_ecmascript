07fc6c3a8a1b33d717dd1a55a5e92d23129049d2
Donovan Rojas <rojasdrm03@gmail.com>
2023-04-14 16:45:22 -0500

    string Padding and trailling commas

---
+++ src/es8/02-string-padding.js
@@ -0,0 +1,7 @@
+const string = 'Hello';
+console.log(string.padStart(7, 'hi'));
+
+//hiHello
+
+console.log(string.padEnd(10, '_'));
+//Hello_____
---
+++ src/es8/03-trailling-commas.js
@@ -0,0 +1,7 @@
+const age = [24,34,35,33, , ,];
+
+console.log(age);
+console.log(age.length);
+
+//[ 24, 34, 35, 33, <2 empty items> ]
+//6
