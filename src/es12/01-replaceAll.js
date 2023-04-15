9e4428a354a7b041767a79f15dc38a6e98563bbd
Donovan Rojas <rojasdrm03@gmail.com>
2023-04-15 13:28:20 -0500

    Es12 numericSeparators and replaceAll

---
+++ src/es12/01-replaceAll.js
@@ -0,0 +1,10 @@
+const string = "JavaScript es un maravilloso lenguaje, con JavaScript puedo..."
+
+const replacedString = string.replace("JavaScript", "TypeScript");
+const replacedString2 = string.replaceAll("JavaScript", "TypeScript");
+
+console.log(replacedString);
+console.log(replacedString2);
+
+//TypeScript es un maravilloso lenguaje, con JavaScript puedo...
+//TypeScript es un maravilloso lenguaje, con TypeScript puedo...
